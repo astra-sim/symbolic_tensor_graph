12ffa479d15e51b7bb274fbca73fe4f0e912649a
Changhai <changhai0109@hotmail.com>
2023-10-02 11:34:59 -0400

    add meta_dataset_generator

---
+++ generate_meta_ext_dataset.py
@@ -0,0 +1,55 @@
+import os, random
+from models.transformer import transformer, transformer_offload_strategy
+from multiprocessing import Pool, cpu_count
+from symbolic2chakra_converter import (
+    Symbolic2ChakraConverter,
+    Symbolic2ChakraConverterWithOffload,
+)
+
+
+def _convert(_symbolic_value_map, symbolic, eg, offload=None):
+    if offload is None:
+        converter = Symbolic2ChakraConverter(
+            symbolic, eg, _symbolic_value_map["bp"] * _symbolic_value_map["mp"]
+        )
+    else:
+        converter = Symbolic2ChakraConverterWithOffload(
+            symbolic,
+            offload,
+            eg,
+            _symbolic_value_map["bp"] * _symbolic_value_map["mp"],
+        )
+    converter.symbol_value_map = _symbolic_value_map
+    converter.convert()
+
+    return True
+
+
+def _sample_symbol_value_map(symbol_value_map_template):
+    symbol_value_map = dict()
+    for key in symbol_value_map_template:
+        if key == "bp":
+            key = 2 ** random.randint(low, high)
+            continue
+        low, high = symbol_value_map_template[key]
+        symbol_value_map[key] = random.randint(low, high)
+    if "B" in symbol_value_map and "bp" in symbol_value_map:
+        symbol_value_map["B"] *= symbol_value_map["bp"]
+    if "bp" in symbol_value_map and "mp" in symbol_value_map:
+        symbol_value_map["mp"] = 256 // symbol_value_map["bp"]
+
+    return symbol_value_map
+
+
+if __name__ == "__main__":
+    symbol_value_map_template = {
+        "bp": (0, 8),
+        "mp": (0, 8),
+        "B": (1, 2048),
+        "Seq": (32, 4096),
+        "H": (4, 1024),
+        "D": (10, 1000),
+        "DF": (40, 4000),
+        "DI": (20, 2000),
+        "DO": (10, 1000),
+    }
--- v9/experiment/dp_dimensions_random/generate_run_files.py
+++ v9/experiment/dp_dimensions_random/generate_run_files.py
@@ -1,7 +1,8 @@
 import sys, os, random, json, time
-sys.path.append('../../')
-sys.path.append('../../../../chakra/et_def/')
-sys.path.append('../../../../chakra/third_party/utils/')
+
+sys.path.append("../../")
+sys.path.append("../../../../chakra/et_def/")
+sys.path.append("../../../../chakra/third_party/utils/")
 
 from multiprocessing import Pool, cpu_count
 
@@ -10,64 +11,68 @@ from models.transformer import transformer
 from symbolic2chakra_converter import Symbolic2ChakraConverter
 
 num_samples = 2048
-et_dir = './run_file/ets'
-symbolic_dir = '../../sharding_spreadsheets/dp'
+et_dir = "./run_file/ets/1"
+symbolic_dir = "../../sharding_spreadsheets/dp"
 os.makedirs(et_dir, exist_ok=True)
-pool = Pool(int(cpu_count()*1))
 
 
-def prepare_templates(design_space_):
+def prepare_templates(design_space_, num_layers_):
+    pool = Pool(int(cpu_count() * 1))
     handler = list()
-    for num_layers in design_space_.num_layers:
+    for num_layers in num_layers_:
         handler.append(pool.apply_async(transformer, (num_layers, symbolic_dir)))
     for h in handler:
         h.get()
     handler.clear()
+    pool.close()
+    pool.join()
     return
 
 
 def sample_symbol_value_map(design_space_):
     symbol_value_map = dict()
-    for key in design_space_.symbol_value_map:
-        value = random.choice(design_space_.symbol_value_map[key])
+    for key in design_space_:
+        value = random.choice(design_space_[key])
         symbol_value_map[key] = value
     return symbol_value_map
-    
 
-def convert_task(i_, design_space_):
+
+def convert_task(i_, design_space_, num_layers):
     os.makedirs(os.path.join(et_dir, f"{i_}"), exist_ok=True)
     random.seed(time.time_ns())
     print(i_)
-    num_layers = random.choice(design_space_.num_layers)
+    num_layers = random.choice(num_layers)
     converter = Symbolic2ChakraConverter(
         os.path.join(symbolic_dir, "processed_graphs", f"transformer_{num_layers}.csv"),
         os.path.join(et_dir, f"{i_}", f"transformer_{i_}"),
-        256
+        256,
     )
     symbol_value_map = sample_symbol_value_map(design_space_)
     print(symbol_value_map)
     converter.symbol_value_map = symbol_value_map
     converter.convert()
-    symbol_value_map['num_layers'] = num_layers
+    symbol_value_map["num_layers"] = num_layers
     return symbol_value_map
 
 
-def convert(design_space_):
+def convert(design_space_, num_layers):
+    pool = Pool(int(cpu_count() * 1))
     cfgs = dict()
     for i in range(num_samples):
-        cfgs[i] = pool.apply_async(convert_task, (i, design_space_))
+        cfgs[i] = pool.apply_async(convert_task, (i, design_space_, num_layers))
     for i in cfgs.keys():
         cfgs[i] = cfgs[i].get()
+    pool.close()
+    pool.join()
     return cfgs
 
 
-if __name__ == '__main__':
-    prepare_templates(design_space_=design_space)
-    cfgs = convert(design_space_=design_space)
-    
-    pool.close()
-    pool.join()
-    
-    f = open('./run_file/cfgs.json', 'w')
+if __name__ == "__main__":
+    design_space_ = design_space.symbol_value_map
+    num_layers = design_space.num_layers
+    prepare_templates(design_space_=design_space_, num_layers_=num_layers)
+    cfgs = convert(design_space_=design_space_, num_layers=num_layers)
+
+    f = open("./run_file/cfgs.json", "w")
     json.dump(cfgs, f)
     f.close()
