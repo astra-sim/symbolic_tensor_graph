e312ca8341171132ad2efde90dda53060c044074
Changhai <changhai0109@hotmail.com>
2023-10-24 15:56:43 -0400

    implemented new scheduler: the baseline one

--- symbolic_tensor_graph/scheduler/scheduler.py
+++ symbolic_tensor_graph/scheduler/scheduler.py
@@ -1,79 +1,211 @@
-import copy
+import copy, os, sys
+import bisect
+
+file_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(file_dir, "../../chakra/et_def/"))
+sys.path.append(os.path.join(file_dir, "../../chakra/third_party/utils/"))
+
+from et_def_pb2 import *
+from protolib import *
 
 
-class SchedulerBase:
-    def __init__(self, eg_nodes, node_runtime=None, num_queue=1, inplace=False):
+class Scheduler:
+    class TaskQueue:
+        def __init__(self, function=None):
+            if function is None:
+                function = (
+                    NodeType.COMP_NODE,
+                    NodeType.COMM_COLL_NODE,
+                    NodeType.COMM_SEND_NODE,
+                    NodeType.COMM_RECV_NODE,
+                    NodeType.MEM_LOAD_NODE,
+                    NodeType.MEM_STORE_NODE,
+                )
+            self.function = function
+            self.tasks = list()  # for each entity (node_id, begin_tick, end_tick)
+
+        def issuable(
+            self,
+            node,
+            earlist_begin_tick=None,
+            latest_finish_tick=None,
+            duration_tick=None,
+        ):
+            if not node.node_type in self.function:
+                return False
+            if earlist_begin_tick is not None:
+                assert latest_finish_tick is not None
+                assert duration_tick is not None
+                _, begin_tick = self.find_first_available_gap(
+                    earlist_begin_tick, duration_tick
+                )
+                end_tick = begin_tick + duration_tick
+                if end_tick > latest_finish_tick:
+                    return False
+            return True
+
+        def insert_task(self, node, begin_tick, duration_tick):
+            # do not allow task with duration of 0
+            assert duration_tick > 0
+
+            gap_index, begin_tick = self.find_first_available_gap(
+                begin_tick, duration_tick
+            )
+            end_tick = begin_tick + duration_tick
+            self.tasks.insert(gap_index, (node.id, begin_tick, end_tick))
+            return begin_tick, end_tick
+
+        def find_first_available_gap(self, begin_tick, duration_tick):
+            # in tasks:
+            # task1 | gap | task2
+            # b1  e1        b2  e
+            #               gap_index
+            # the available gap should be b >= e1 and e <= b2
+            gap_index = bisect.bisect(self.tasks, begin_tick, key=lambda t: t[2])
+            while True:
+                if gap_index > 0:
+                    gap_begin = self.tasks[gap_index - 1][2]
+                else:
+                    gap_begin = 0
+
+                if gap_index < len(self.tasks):
+                    gap_end = self.tasks[gap_index][1]
+                else:
+                    gap_end = float("inf")
+
+                if gap_end >= gap_begin + duration_tick:
+                    # gap has enough space
+                    break
+
+                if gap_index >= len(self.tasks):
+                    # find to end of queue, and the gap after the last task should be large enough so shouldnt be here
+                    assert False
+                gap_index += 1
+            return gap_index, gap_begin
+
+        def sanity_check(self):
+            end_tick = -float("inf")
+            for task in self.tasks:
+                assert task[1] >= end_tick
+                assert task[2] > task[1]
+                end_tick = task[2]
+
+        @property
+        def latest_task_tick(self):
+            if len(self.tasks) > 0:
+                return self.tasks[-1][2]
+            else:
+                return -float("inf")
+
+    def __init__(
+        self, eg_nodes, node_runtime=None, queues_function=None, inplace=False
+    ):
         if inplace:
             self.nodes = eg_nodes
         else:
             self.nodes = copy.deepcopy(eg_nodes)
         self.node_runtime = node_runtime
-        self.num_queue = num_queue
 
         self.queues = list()
-        self.queues_tick = list()
-        for _ in range(self.num_queue):
-            self.queues.append(list())
-            self.queues_tick.append(0)
+        if queues_function is None:
+            queues_function = [None]
+        for queue_function in queues_function:
+            self.queues.append(Scheduler.TaskQueue(queue_function))
 
-        self._id_node_map = None
-        self._parent_child_map = None
-
-    def issue_node(self, node):
-        if self.node_runtime == None:
-            delta_tick = 1
-        else:
-            assert node.id in self.node_runtime
-            delta_tick = self.node_runtime[node.id]
-
-        min_queue_tick = 1e90
-        min_queue = None
-        min_queue_index = -1
-        for i, (queue, queue_tick) in enumerate(zip(self.queues, self.queues_tick)):
-            if queue_tick < min_queue_tick:
-                min_queue = queue
-                min_queue_index = i
-                min_queue_tick = min(min_queue_tick, queue_tick)
-        # print(
-        #     f"issue node={node.name} queue={min_queue_index} begin_queue_tick={min_queue_tick} after_queue_tick={min_queue_tick+delta_tick}"
-        # )
-        min_queue.append(node)
-        self.queues_tick[min_queue_index] += delta_tick
+        self._node_id_map_node = None
+        self._parent_map_child = None
 
     @property
-    def id_node_map(self):
-        if self._id_node_map is not None:
-            if len(self._id_node_map) != len(self.nodes):
-                self._id_node_map = None
-        if self._id_node_map is None:
-            self._id_node_map = dict()
+    def node_id_map_node(self):
+        if self._node_id_map_node is not None:
+            if len(self._node_id_map_node) != len(self.nodes):
+                self._node_id_map_node = None
+        if self._node_id_map_node is None:
+            self._node_id_map_node = dict()
             for node in self.nodes:
-                self._id_node_map[node.id] = node
-        return self._id_node_map
+                self._node_id_map_node[node.id] = node
+        return self._node_id_map_node
 
     @property
-    def parent_child_map(self):
-        if self._parent_child_map is not None:
-            if len(self._parent_child_map) != len(self.nodes):
-                self._parent_child_map = None
-        if self._parent_child_map == None:
-            self._parent_child_map = dict()
+    def parent_map_child(self):
+        if self._parent_map_child is not None:
+            if len(self._parent_map_child) != len(self.nodes):
+                self._parent_map_child = None
+        if self._parent_map_child == None:
+            self._parent_map_child = dict()
             for node in self.nodes:
-                self._parent_child_map[node.id] = list()
+                self._parent_map_child[node.id] = list()
             for node in self.nodes:
                 for parent in node.parent:
-                    self._parent_child_map[parent].append(node.id)
-        return self._parent_child_map
+                    self._parent_map_child[parent].append(node.id)
+        return self._parent_map_child
+
+    def get_node_runtime(self, node):
+        if self.node_runtime is None:
+            return 1
+        return self.node_runtime[node.id]
 
     def resolve_queue(self):
-        raise NotImplementedError()
+        for queue in self.queues:
+            queue.tasks.clear()
+        pending_issue_parents = dict()
+        node_id_map_end_time = dict()
+        for node in self.nodes:
+            pending_issue_parents[node.id] = list()
+            for parent in node.parent:
+                pending_issue_parents[node.id].append(parent)
+        dep_free_nodes = list()
+        for node in self.nodes:
+            if len(pending_issue_parents[node.id]) == 0:
+                dep_free_nodes.append(node)
+
+        while not len(dep_free_nodes) == 0:
+            freed_nodes_this_round = list()
+            for node in dep_free_nodes:
+                latest_parent_end_time = 0
+                for parent in node.parent:
+                    assert (
+                        parent in node_id_map_end_time
+                    )  # as it is dep free, all parent should finished and have end time
+                    latest_parent_end_time = max(
+                        latest_parent_end_time, node_id_map_end_time[parent]
+                    )
+                duration_time = self.get_node_runtime(node)
+                begin_time = latest_parent_end_time
+                # select a queue, here just find the one with shortest latest tick
+                shortest_queue_tick = float("inf")
+                shortest_queue = None
+                for queue in self.queues:
+                    if not queue.issuable(node):
+                        continue
+                    if queue.latest_task_tick < shortest_queue_tick:
+                        shortest_queue_tick = queue.latest_task_tick
+                        shortest_queue = queue
+                # if the shortest queue is none, then means no queue in system can handle this node type
+                assert shortest_queue is not None
+                begin_time, end_time = shortest_queue.insert_task(
+                    node, begin_time, duration_time
+                )
+                print(
+                    f"insert {node.name} in queue {id(queue)} and with {begin_time}:{end_time}"
+                )
+                node_id_map_end_time[node.id] = end_time
+                for child in self.parent_map_child[node.id]:
+                    assert node.id in pending_issue_parents[child]
+                    pending_issue_parents[child].remove(node.id)
+                    if len(pending_issue_parents[child]) == 0:
+                        freed_nodes_this_round.append(self.node_id_map_node[child])
+            dep_free_nodes = freed_nodes_this_round
+
+        assert len(node_id_map_end_time) == len(self.nodes)
 
     def apply(self):
         self.resolve_queue()
         for queue in self.queues:
-            for i in range(len(queue) - 1):
-                from_ = queue[i]
-                to_ = queue[i + 1]
-                if not from_.id in to_.parent:
-                    to_.parent.append(from_.id)
+            for i in range(len(queue.tasks) - 1):
+                from_id = queue.tasks[i][0]
+                to_id = queue.tasks[i + 1][0]
+                to_ = self.node_id_map_node[to_id]
+                if not from_id in to_.parent:
+                    to_.parent.append(from_id)
         return self.nodes
--- verify_greedy_scheduler_removal.py
+++ verify_greedy_scheduler_removal.py
@@ -1,6 +1,8 @@
 import copy
 from symbolic_tensor_graph.chakra_remove_shortcuts import ChakraShortcutRemover
-from symbolic_tensor_graph.scheduler.greedy_scheduler import GreedyScheduler
+from symbolic_tensor_graph.scheduler.scheduler import Scheduler as GreedyScheduler
+
+# from symbolic_tensor_graph.scheduler.greedy_scheduler import GreedyScheduler
 from symbolic_tensor_graph.symbolic2chakra_converter import Symbolic2ChakraConverter
 from eg_simulator.executor.astrasim_executor import AstraSIMExecutor
 from eg_simulator.runtime_database.astrasim_runtime_database import (
@@ -50,113 +52,130 @@ if __name__ == "__main__":
         assert not node.id in nodes_runtime
         nodes_runtime[node.id] = node_runtime
 
-    converter.readout()
+    # converter.readout()
 
-    greedy_scheduler = GreedyScheduler(copy.deepcopy(ori_nodes))
-    g1_nodes = greedy_scheduler.apply()
-    converter.replace_nodes(g1_nodes)
-    converter.eg_file = "sharding_spreadsheets/transformer/validation/symbolic_transformer2.w0l0i0.dp.g1"
-    converter.readout()
+    # greedy_scheduler = GreedyScheduler(copy.deepcopy(ori_nodes))
+    # g1_nodes = greedy_scheduler.apply()
+    # converter.replace_nodes(g1_nodes)
+    # converter.eg_file = "sharding_spreadsheets/transformer/validation/symbolic_transformer2.w0l0i0.dp.g1"
+    # converter.readout()
 
-    greedy_scheduler = GreedyScheduler(copy.deepcopy(ori_nodes), num_queue=2)
-    g2_nodes = greedy_scheduler.apply()
-    converter.replace_nodes(g2_nodes)
-    converter.eg_file = "sharding_spreadsheets/transformer/validation/symbolic_transformer2.w0l0i0.dp.g2"
-    converter.readout()
+    # greedy_scheduler = GreedyScheduler(
+    #     copy.deepcopy(ori_nodes), queues_function=[None, None]
+    # )
+    # g2_nodes = greedy_scheduler.apply()
+    # converter.replace_nodes(g2_nodes)
+    # converter.eg_file = "sharding_spreadsheets/transformer/validation/symbolic_transformer2.w0l0i0.dp.g2"
+    # converter.readout()
 
-    greedy_scheduler = GreedyScheduler(copy.deepcopy(ori_nodes), num_queue=3)
-    g3_nodes = greedy_scheduler.apply()
-    converter.replace_nodes(g3_nodes)
-    converter.eg_file = "sharding_spreadsheets/transformer/validation/symbolic_transformer2.w0l0i0.dp.g3"
-    converter.readout()
+    # greedy_scheduler = GreedyScheduler(
+    #     copy.deepcopy(ori_nodes), queues_function=[None, None, None]
+    # )
+    # g3_nodes = greedy_scheduler.apply()
+    # converter.replace_nodes(g3_nodes)
+    # converter.eg_file = "sharding_spreadsheets/transformer/validation/symbolic_transformer2.w0l0i0.dp.g3"
+    # converter.readout()
 
-    greedy_scheduler = GreedyScheduler(copy.deepcopy(ori_nodes), num_queue=5)
-    g5_nodes = greedy_scheduler.apply()
-    converter.replace_nodes(g5_nodes)
-    converter.eg_file = "sharding_spreadsheets/transformer/validation/symbolic_transformer2.w0l0i0.dp.g5"
-    converter.readout()
+    # greedy_scheduler = GreedyScheduler(
+    #     copy.deepcopy(ori_nodes), queues_function=[None, None, None, None, None]
+    # )
+    # g5_nodes = greedy_scheduler.apply()
+    # converter.replace_nodes(g5_nodes)
+    # converter.eg_file = "sharding_spreadsheets/transformer/validation/symbolic_transformer2.w0l0i0.dp.g5"
+    # converter.readout()
 
-    greedy_scheduler = GreedyScheduler(copy.deepcopy(ori_nodes), num_queue=10)
-    g10_nodes = greedy_scheduler.apply()
-    converter.replace_nodes(g10_nodes)
-    converter.eg_file = "sharding_spreadsheets/transformer/validation/symbolic_transformer2.w0l0i0.dp.g10"
-    converter.readout()
+    # greedy_scheduler = GreedyScheduler(
+    #     copy.deepcopy(ori_nodes),
+    #     queues_function=[None, None, None, None, None, None, None, None, None, None],
+    # )
+    # g10_nodes = greedy_scheduler.apply()
+    # converter.replace_nodes(g10_nodes)
+    # converter.eg_file = "sharding_spreadsheets/transformer/validation/symbolic_transformer2.w0l0i0.dp.g10"
+    # converter.readout()
 
-    shortcut_remover = ChakraShortcutRemover(copy.deepcopy(g1_nodes))
-    g1s_nodes = shortcut_remover.apply()
-    converter.replace_nodes(g1s_nodes)
-    converter.eg_file = "sharding_spreadsheets/transformer/validation/symbolic_transformer2.w0l0i0.dp.g1s"
-    converter.readout()
+    # shortcut_remover = ChakraShortcutRemover(copy.deepcopy(g1_nodes))
+    # g1s_nodes = shortcut_remover.apply()
+    # converter.replace_nodes(g1s_nodes)
+    # converter.eg_file = "sharding_spreadsheets/transformer/validation/symbolic_transformer2.w0l0i0.dp.g1s"
+    # converter.readout()
 
-    shortcut_remover = ChakraShortcutRemover(copy.deepcopy(g2_nodes))
-    g2s_nodes = shortcut_remover.apply()
-    converter.replace_nodes(g2s_nodes)
-    converter.eg_file = "sharding_spreadsheets/transformer/validation/symbolic_transformer2.w0l0i0.dp.g2s"
-    converter.readout()
+    # shortcut_remover = ChakraShortcutRemover(copy.deepcopy(g2_nodes))
+    # g2s_nodes = shortcut_remover.apply()
+    # converter.replace_nodes(g2s_nodes)
+    # converter.eg_file = "sharding_spreadsheets/transformer/validation/symbolic_transformer2.w0l0i0.dp.g2s"
+    # converter.readout()
 
-    shortcut_remover = ChakraShortcutRemover(copy.deepcopy(g3_nodes))
-    g3s_nodes = shortcut_remover.apply()
-    converter.replace_nodes(g3s_nodes)
-    converter.eg_file = "sharding_spreadsheets/transformer/validation/symbolic_transformer2.w0l0i0.dp.g3s"
-    converter.readout()
+    # shortcut_remover = ChakraShortcutRemover(copy.deepcopy(g3_nodes))
+    # g3s_nodes = shortcut_remover.apply()
+    # converter.replace_nodes(g3s_nodes)
+    # converter.eg_file = "sharding_spreadsheets/transformer/validation/symbolic_transformer2.w0l0i0.dp.g3s"
+    # converter.readout()
 
-    shortcut_remover = ChakraShortcutRemover(copy.deepcopy(g5_nodes))
-    g5s_nodes = shortcut_remover.apply()
-    converter.replace_nodes(g5s_nodes)
-    converter.eg_file = "sharding_spreadsheets/transformer/validation/symbolic_transformer2.w0l0i0.dp.g5s"
-    converter.readout()
+    # shortcut_remover = ChakraShortcutRemover(copy.deepcopy(g5_nodes))
+    # g5s_nodes = shortcut_remover.apply()
+    # converter.replace_nodes(g5s_nodes)
+    # converter.eg_file = "sharding_spreadsheets/transformer/validation/symbolic_transformer2.w0l0i0.dp.g5s"
+    # converter.readout()
 
-    shortcut_remover = ChakraShortcutRemover(copy.deepcopy(g10_nodes))
-    g10s_nodes = shortcut_remover.apply()
-    converter.replace_nodes(g10s_nodes)
-    converter.eg_file = "sharding_spreadsheets/transformer/validation/symbolic_transformer2.w0l0i0.dp.g10s"
-    converter.readout()
+    # shortcut_remover = ChakraShortcutRemover(copy.deepcopy(g10_nodes))
+    # g10s_nodes = shortcut_remover.apply()
+    # converter.replace_nodes(g10s_nodes)
+    # converter.eg_file = "sharding_spreadsheets/transformer/validation/symbolic_transformer2.w0l0i0.dp.g10s"
+    # converter.readout()
+
+    # greedy_scheduler = GreedyScheduler(
+    #     copy.deepcopy(ori_nodes), node_runtime=nodes_runtime
+    # )
+    # g1_nodes = greedy_scheduler.apply()
+    # converter.replace_nodes(g1_nodes)
+    # converter.eg_file = "sharding_spreadsheets/transformer/validation/symbolic_transformer2.w0l0i0.dp.gr1"
+    # converter.readout()
 
     greedy_scheduler = GreedyScheduler(
-        copy.deepcopy(ori_nodes), node_runtime=nodes_runtime
-    )
-    g1_nodes = greedy_scheduler.apply()
-    converter.replace_nodes(g1_nodes)
-    converter.eg_file = "sharding_spreadsheets/transformer/validation/symbolic_transformer2.w0l0i0.dp.gr1"
-    converter.readout()
-
-    greedy_scheduler = GreedyScheduler(
-        copy.deepcopy(ori_nodes), node_runtime=nodes_runtime, num_queue=2
+        copy.deepcopy(ori_nodes),
+        node_runtime=nodes_runtime,
+        queues_function=[None, None],
     )
     g2_nodes = greedy_scheduler.apply()
     converter.replace_nodes(g2_nodes)
     converter.eg_file = "sharding_spreadsheets/transformer/validation/symbolic_transformer2.w0l0i0.dp.gr2"
     converter.readout()
 
-    greedy_scheduler = GreedyScheduler(
-        copy.deepcopy(ori_nodes), node_runtime=nodes_runtime, num_queue=3
-    )
-    g3_nodes = greedy_scheduler.apply()
-    converter.replace_nodes(g3_nodes)
-    converter.eg_file = "sharding_spreadsheets/transformer/validation/symbolic_transformer2.w0l0i0.dp.gr3"
-    converter.readout()
+    # greedy_scheduler = GreedyScheduler(
+    #     copy.deepcopy(ori_nodes),
+    #     node_runtime=nodes_runtime,
+    #     queues_function=[None, None, None],
+    # )
+    # g3_nodes = greedy_scheduler.apply()
+    # converter.replace_nodes(g3_nodes)
+    # converter.eg_file = "sharding_spreadsheets/transformer/validation/symbolic_transformer2.w0l0i0.dp.gr3"
+    # converter.readout()
 
-    greedy_scheduler = GreedyScheduler(
-        copy.deepcopy(ori_nodes), node_runtime=nodes_runtime, num_queue=5
-    )
-    g5_nodes = greedy_scheduler.apply()
-    converter.replace_nodes(g5_nodes)
-    converter.eg_file = "sharding_spreadsheets/transformer/validation/symbolic_transformer2.w0l0i0.dp.gr5"
-    converter.readout()
+    # greedy_scheduler = GreedyScheduler(
+    #     copy.deepcopy(ori_nodes),
+    #     node_runtime=nodes_runtime,
+    #     queues_function=[None, None, None, None, None],
+    # )
+    # g5_nodes = greedy_scheduler.apply()
+    # converter.replace_nodes(g5_nodes)
+    # converter.eg_file = "sharding_spreadsheets/transformer/validation/symbolic_transformer2.w0l0i0.dp.gr5"
+    # converter.readout()
 
-    greedy_scheduler = GreedyScheduler(
-        copy.deepcopy(ori_nodes), node_runtime=nodes_runtime, num_queue=10
-    )
-    g10_nodes = greedy_scheduler.apply()
-    converter.replace_nodes(g10_nodes)
-    converter.eg_file = "sharding_spreadsheets/transformer/validation/symbolic_transformer2.w0l0i0.dp.gr10"
-    converter.readout()
+    # greedy_scheduler = GreedyScheduler(
+    #     copy.deepcopy(ori_nodes),
+    #     node_runtime=nodes_runtime,
+    #     queues_function=[None, None, None, None, None, None, None, None, None, None],
+    # )
+    # g10_nodes = greedy_scheduler.apply()
+    # converter.replace_nodes(g10_nodes)
+    # converter.eg_file = "sharding_spreadsheets/transformer/validation/symbolic_transformer2.w0l0i0.dp.gr10"
+    # converter.readout()
 
-    shortcut_remover = ChakraShortcutRemover(copy.deepcopy(g1_nodes))
-    g1s_nodes = shortcut_remover.apply()
-    converter.replace_nodes(g1s_nodes)
-    converter.eg_file = "sharding_spreadsheets/transformer/validation/symbolic_transformer2.w0l0i0.dp.gr1s"
-    converter.readout()
+    # shortcut_remover = ChakraShortcutRemover(copy.deepcopy(g1_nodes))
+    # g1s_nodes = shortcut_remover.apply()
+    # converter.replace_nodes(g1s_nodes)
+    # converter.eg_file = "sharding_spreadsheets/transformer/validation/symbolic_transformer2.w0l0i0.dp.gr1s"
+    # converter.readout()
 
     shortcut_remover = ChakraShortcutRemover(copy.deepcopy(g2_nodes))
     g2s_nodes = shortcut_remover.apply()
@@ -164,20 +183,20 @@ if __name__ == "__main__":
     converter.eg_file = "sharding_spreadsheets/transformer/validation/symbolic_transformer2.w0l0i0.dp.gr2s"
     converter.readout()
 
-    shortcut_remover = ChakraShortcutRemover(copy.deepcopy(g3_nodes))
-    g3s_nodes = shortcut_remover.apply()
-    converter.replace_nodes(g3s_nodes)
-    converter.eg_file = "sharding_spreadsheets/transformer/validation/symbolic_transformer2.w0l0i0.dp.gr3s"
-    converter.readout()
+    # shortcut_remover = ChakraShortcutRemover(copy.deepcopy(g3_nodes))
+    # g3s_nodes = shortcut_remover.apply()
+    # converter.replace_nodes(g3s_nodes)
+    # converter.eg_file = "sharding_spreadsheets/transformer/validation/symbolic_transformer2.w0l0i0.dp.gr3s"
+    # converter.readout()
 
-    shortcut_remover = ChakraShortcutRemover(copy.deepcopy(g5_nodes))
-    g5s_nodes = shortcut_remover.apply()
-    converter.replace_nodes(g5s_nodes)
-    converter.eg_file = "sharding_spreadsheets/transformer/validation/symbolic_transformer2.w0l0i0.dp.gr5s"
-    converter.readout()
+    # shortcut_remover = ChakraShortcutRemover(copy.deepcopy(g5_nodes))
+    # g5s_nodes = shortcut_remover.apply()
+    # converter.replace_nodes(g5s_nodes)
+    # converter.eg_file = "sharding_spreadsheets/transformer/validation/symbolic_transformer2.w0l0i0.dp.gr5s"
+    # converter.readout()
 
-    shortcut_remover = ChakraShortcutRemover(copy.deepcopy(g10_nodes))
-    g10s_nodes = shortcut_remover.apply()
-    converter.replace_nodes(g10s_nodes)
-    converter.eg_file = "sharding_spreadsheets/transformer/validation/symbolic_transformer2.w0l0i0.dp.gr10s"
-    converter.readout()
+    # shortcut_remover = ChakraShortcutRemover(copy.deepcopy(g10_nodes))
+    # g10s_nodes = shortcut_remover.apply()
+    # converter.replace_nodes(g10s_nodes)
+    # converter.eg_file = "sharding_spreadsheets/transformer/validation/symbolic_transformer2.w0l0i0.dp.gr10s"
+    # converter.readout()
